86b91dfebab1032774239422cf136c9a512b52cf
Edwin Portillo <weaselssh@gmail.com>
2025-08-01 11:57:16 -0600

    ahora si

--- EdwinPortilloThomasEduardoLab2/src/edwinportillothomaseduardolab2/EdwinPortilloThomasEduardoLab2.java
+++ EdwinPortilloThomasEduardoLab2/src/edwinportillothomaseduardolab2/EdwinPortilloThomasEduardoLab2.java
@@ -1,9 +1,13 @@
 package edwinportillothomaseduardolab2;
 
+import javax.swing.SwingUtilities;
+
 public class EdwinPortilloThomasEduardoLab2 {
 
     public static void main(String[] args) {
-        
+        SwingUtilities.invokeLater(() -> {
+            new MenuPrincipal().setVisible(true);
+        });
     }
-    
+
 }
